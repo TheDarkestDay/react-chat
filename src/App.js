90de62534261926c6ea400e46c0f51594037c8ee
AlexanderBrenchev <alexzund3r@yahoo.com>
2018-03-04 19:29:05 +0700

    History ejected from BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
+import { Router, Redirect, Route, Switch } from 'react-router-dom';
 import Reboot from 'material-ui/Reboot';
 import 'typeface-roboto';
 import { Provider } from 'react-redux';
 
+import history from './utils/history';
 import store from './store';
 import AuthPage from './containers/AuthPage';
 import ChatPage from './components/ChatPage';
@@ -13,7 +14,7 @@ class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <Router>
+        <Router history={history}>
           <React.Fragment>
             <Reboot />
             <Switch>
