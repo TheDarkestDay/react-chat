643a55e1a37a3d874d4f3fe7b90e62a574414844
Aleksandr Brenchev <Aleksandr.Brenchev@sidenis.com>
2018-03-25 22:00:41 -0700

    More tests for reducers

--- src/reducers/auth.test.js
+++ src/reducers/auth.test.js
@@ -2,6 +2,8 @@
 import reducer from './auth';
 import * as ActionType from '../constants/action-types';
 
+import { userMock } from '../mocks';
+
 describe('auth reducer', () => {
   it('should return initial state', () => {
     expect(reducer(undefined, {})).toEqual({
@@ -14,6 +16,44 @@ describe('auth reducer', () => {
     expect(reducer({ isAuthenticated: true, user: {} }, { type: ActionType.LOGOUT })).toEqual({
       isAuthenticated: false,
       user: null,
+    });
+  });
+
+  const authSuccessActions = [
+    ActionType.LOGIN_SUCCESS,
+    ActionType.SIGNUP_SUCCESS,
+    ActionType.WHOAMI_SUCCESS,
+  ];
+
+  authSuccessActions.forEach((action) => {
+    it(`should authenticate user after ${action} action`, () => {
+      expect(reducer(undefined, { type: action, payload: userMock })).toEqual({
+        isAuthenticated: true,
+        user: userMock,
+      });
+    });
+  });
+
+  it('should update user info after EDIT_USER_SUCCESS action', () => {
+    const editedUserMock = {
+      _id: '1',
+      username: 'test2',
+      firstName: 'John',
+      lastName: 'Doe',
+    };
+
+    expect(reducer(
+      {
+        isAuthenticated: true,
+        user: userMock,
+      },
+      {
+        type: ActionType.EDIT_USER_SUCCESS,
+        payload: editedUserMock,
+      },
+    )).toEqual({
+      isAuthenticated: true,
+      user: editedUserMock,
     });
   });
 });
