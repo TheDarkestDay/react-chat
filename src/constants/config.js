d97b3a83bb16b21a24ad3ef41679d7a38a2d2482
AlexanderBrenchev <alexzund3r@yahoo.com>
2018-03-28 22:37:15 +0700

    Renamed ENV vars

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,2 +1,2 @@
-export const API_ROOT = process.env.API_ROOT || 'http://localhost:8080/v1';
-export const WS_ROOT = process.env.SOCKETS_ROOT || 'ws://localhost:8080';
+export const API_ROOT = process.env.REACT_APP_API_ROOT || 'http://localhost:8080/v1';
+export const WS_ROOT = process.env.REACT_APP_SOCKETS_ROOT || 'ws://localhost:8080';
