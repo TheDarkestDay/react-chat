7192af47ef9c68f06b308234592ba3b9c9ab457f
AlexanderBrenchev <alexzund3r@yahoo.com>
2018-03-28 23:09:06 +0700

    Broken test for SignupForm fixed

--- src/components/SignupForm.test.js
+++ src/components/SignupForm.test.js
@@ -5,6 +5,6 @@ import SignupForm from './SignupForm';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<SignupForm signup={() => {}} />, div);
+  ReactDOM.render(<SignupForm signup={() => {}} signupError={() => {}} />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
